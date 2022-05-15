2e1c278e41a159e00ef579c52486fe74e61d757f
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2022-05-15 15:22:01 +0100

    Unit 13 complete

---
+++ Hashtables/PhoneBookEntry.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hashtables;
+
+public class PhoneBookEntry
+{
+    public string Surname { get; }
+    public string Forename { get; }
+    public string FullName => $"{Surname} {Forename}";
+
+    private string? _phoneNumber;
+    public string? PhoneNumber
+    {
+        get => _phoneNumber;
+        set
+        {
+            // Ensure it is a well-formatted phone number before setting.
+            if (new PhoneAttribute().IsValid(value))
+                _phoneNumber = value;
+        }
+    }
+    
+    public string? EmailAddress { get; set; }
+    
+    public string? Address { get; set; }
+
+    public PhoneBookEntry(string surname, string forename, string phoneNumber)
+    {
+        Surname = surname;
+        Forename = forename;
+        PhoneNumber = phoneNumber;
+    }
+
+    public override string ToString()
+    {
+        return $"{FullName} - {PhoneNumber}";
+    }
+}
---
+++ Hashtables/User.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hashtables;
+
+public class User
+{
+    public PhoneBook PhoneBook { get; }
+
+    public User()
+    {
+        PhoneBook = new PhoneBook();
+    }
+
+    public void DefaultInsertions()
+    {
+        PhoneBook.Insert(new PhoneBookEntry("Hall", "Liam", "lol")
+        {
+            EmailAddress = "liamhall@outlook.com",
+            Address = "55, HD5 6PL"
+        });
+        
+        PhoneBook.Insert(new PhoneBookEntry("Sinla", "Nari", "+447862485764")
+        {
+            EmailAddress = "narsinla@gmail.com",
+            Address = "87, LS3 1FF"
+        });
+        
+        PhoneBook.Insert(new PhoneBookEntry("Conhee", "Drea", "07538332719")
+        {
+            EmailAddress = "andreaconhee@hotmail.co.uk",
+            Address = "41a, HD3 2EU"
+        });
+    }
+    
+    public void DefaultModifications()
+    {
+        PhoneBook.Insert(new PhoneBookEntry("Hall", "Liam", "+44 7462 457563")
+        {
+            EmailAddress = "liamhall@outlook.com",
+            Address = "55, HD5 6PL"
+        });
+        
+        PhoneBook.Insert(new PhoneBookEntry("Sinla", "Nari", "+447862485764")
+        {
+            EmailAddress = "narsinla@gmail.com",
+            Address = "32, LS5 4NF"
+        });
+
+        PhoneBook.Get("Conhee Drea")!.PhoneNumber = "07575865429";
+    }
+    
+    public void DefaultDeletions()
+    {
+        PhoneBook.Delete("Hall Liam");
+
+        PhoneBook.Delete("Conhee Drea");
+    }
+}
