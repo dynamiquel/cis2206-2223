3bb94f5158bbcdc7d1353ffbfb10e39431b2ae56
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2022-07-14 13:08:53 +0100

    No idea why this didn't commit.

---
+++ Sierpinski/Program.cs
@@ -0,0 +1,11 @@
+﻿using Sierpinski;
+
+Console.WriteLine("Hello, World!");
+
+var carpet = new SierpinskiCarpet(DimensionSize.Nine);
+Console.WriteLine(carpet.ToString());
+
+var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+    "SierpinskiCarpet.txt");
+
+File.WriteAllTextAsync(path, carpet.ToString());
